8d0846f3a51d2f9a963a04ba0ddcd6de6940d02c
박혜미 <haymee@naturemobility.com>
2020-06-12 06:35:11 +0900

    practice gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import pug from 'gulp-pug';
 import del from 'del';
+import ghPages from 'gulp-gh-pages';
 
 const routes = {
   pug: {
@@ -11,8 +12,11 @@ const routes = {
 
 const clean = () => del(['build']);
 const view = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest(routes.pug.dest));
+const gh = () => gulp.src('/build/**/*').pipe(ghPages());
 
 const prepare = gulp.series([clean]);
 const assets = gulp.series([view]);
 
 export const dev = gulp.series([prepare, assets]);
+export const build = gulp.series([prepare, assets]);
+export const deploy = gulp.series([build, gh]);
