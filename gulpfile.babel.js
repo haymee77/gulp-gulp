e29a27363a4b38fbf33c2b42b8eff7f74b471a88
박혜미 <haymee@naturemobility.com>
2020-06-13 00:14:22 +0900

    practice gulp pug, img, scss.. etc.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,11 @@ import del from 'del';
 import ws from 'gulp-webserver';
 import ghPages from 'gulp-gh-pages';
 import gimg from 'gulp-image';
+import gsass from 'gulp-sass';
+import gautoprefixer from 'gulp-autoprefixer';
+import minifyCss from 'gulp-csso';
+
+gsass.compiler = require('node-sass');
 
 const routes = {
   pug: {
@@ -12,24 +17,39 @@ const routes = {
     dest: 'build',
   },
   img: {
-    src: 'src/img/**/*.*',
+    src: 'src/img/*',
     dest: 'build/img',
+  },
+  style: {
+    watch: 'src/scss/*',
+    src: 'src/scss/style.scss',
+    dest: 'build/css',
   },
 };
 
 const clean = () => del(['build']);
 const view = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest(routes.pug.dest));
 const img = () => gulp.src(routes.img.src).pipe(gimg()).pipe(gulp.dest(routes.img.dest));
+const sass = () =>
+  gulp
+    .src(routes.style.src)
+    .pipe(gsass().on('error', gsass.logError))
+    .pipe(gautoprefixer({ cascade: true, overrideBrowserslist: ['last 2 versions'] }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(routes.style.dest));
 
 const webserver = () => gulp.src('build').pipe(ws({ livereload: true, open: true }));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, view);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.style.watch, sass);
 };
 
-const gh = () => gulp.src('/build/**/*').pipe(ghPages());
+const gh = () => gulp.src('build/**/*').pipe(ghPages());
 
-const prepare = gulp.series([clean]);
-const assets = gulp.series([view, img]);
+const prepare = gulp.series([clean, img]);
+const assets = gulp.series([view, sass]);
 const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
